ccd9719d090981010bd6a25466ae39a573530b4c
Liana <smartghil4@gmail.com>
2024-09-29 23:07:52 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Counter from "./component/counter";
+import ClassCounter from './component/class.Counter';
+import './styles/App.css';
+import PostItem from './component/Postitem';
+
 function App() {
-   console.log(1)
   return (
     <div className="App">
-      <Counter/>
+    <PostItem post={{id: 1, title: 'JavaScript', body:'Description'}}/>
     </div>
   );
 }
