10eafee98e309e6db32866696fe564a5644b965d
Liana <smartghil4@gmail.com>
2024-10-27 23:50:27 +0200

    new

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PostForm from './component/PostForm';
 import PostList from './component/PostLists';
 import MyButton from './component/UI/button/MyButton';
 import MyInput from './component/UI/input/MyInput';
+import MySelect from './component/UI/select/MySelect';
 
 function App() {
 
@@ -12,6 +13,18 @@ function App() {
     {id: 2, title: 'JavaScript 2', body:'Description'},
     {id: 3, title: 'JavaScript 3', body:'Description'},
   ])
+ const[selectedSort, setSelectedSort] = useState('')
+ const [searchQuery, setSearchQuery] = useState('')
+
+ function getSortedPosts(){
+  console.log('щось спрацювало')
+   if (selectedSort){
+   return [...posts].sort( (a, b) => a [selectedSort].localeCompare(b[selectedSort]) )
+   }
+   return posts;
+ }
+
+ const sortedPosts = getSortedPosts()
  
   const createPost = (newPost) => {
     setPosts([...posts,newPost])
@@ -21,12 +34,34 @@ const removePost = (post) => {
 setPosts(posts.filter(p => p.id !== post.id))
 }
 
+const sortPosts = (sort) => {
+  setSelectedSort(sort);
+  
+}
   return (
     <div className="App">
       <PostForm create={createPost} />
+      <hr style={{margin: '15 px 8'}}/>
+       <div>
+        <MyInput 
+        value={searchQuery}
+        onChange={ e => setSearchQuery(e.target.value)}
+        placeholder="Search..."
+        />
+        <MySelect
+        value={selectedSort}
+        onChange={sortPosts}
+        defaultValue="сортировка"
+        options={[
+          {value: 'title', name: 'по названию'},
+          {value: 'body', name: 'по описанию'},
+        ]}
+        />
+       </div>
+
       {posts.length !== 0
       ? 
-      <PostList remove={removePost} posts={posts} title = "List 1"/>
+      <PostList remove={removePost} posts={sortedPosts} title = "List 1"/>
     : 
     <h1 style={{textAlign: 'center'}} > 
     Posts are not found
