0a061acabb3d513dcb1f3331b9f8bfce331522d4
Liana <smartghil4@gmail.com>
2024-11-12 02:52:05 +0200

    new

--- src/component/PostLists.jsx
+++ src/component/PostLists.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 // @ts-ignore
 import PostItem from './PostItem';
+import { TransitionGroup } from 'react-transition-group';
+import { CSSTransition } from 'react-transition-group';
 
 const PostList = ({posts, title, remove}) => {
 
@@ -17,10 +19,17 @@ if(!posts.length){
  <h1 style = {{textAlign: 'center'}}>
       {title}
       </h1>
+      <TransitionGroup>
     {posts.map((post, index)  => 
-    <PostItem remove={remove} number = {index + 1} post ={post}  key={post.id}/>
-
+    <CSSTransition
+    key={post.id}
+    timeout={500}
+    className="post"
+    >
+    <PostItem remove={remove} number = {index + 1} post ={post}/>
+</CSSTransition>
    )}
+   </TransitionGroup>
     </div>
   )
 }
