14cec38b13e028454ded1ac01f5b7461add0f653
Liana <smartghil4@gmail.com>
2025-02-21 21:57:37 +0200

    new

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -11,4 +11,11 @@ export default class PostService{//об'єкт
       return response;
    
   }
+
+
+static async getById(id){//метод
+  const response = await axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
+  return response;
+
+}
 }
--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 
 const PostIdPage = () => {
+  const params = useParams()
+  
  return(
 
    
-<h1>вы открыли страницу</h1>
+<h1>вы открыли страницу с ID ={params.id} </h1>
     
   )
 
